d34a38486564db24896eebb8e251d3742c6218ed
Windiarta <windiarta.widjaja@gmail.com>
2026-01-17 10:33:03 +0700

    feat(database): update environment configuration for production; add PostgreSQL adapter and dependencies

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,21 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-const globalForPrisma = global as unknown as {
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter, // Pass the adapter here
     log: ['error', 'warn'],
   });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
-
